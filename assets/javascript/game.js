b815459c0c3a61455e4edca9e3f6e631da29e024
Matthew Bower <mttbwr91@gmail.com>
2018-11-17 15:21:56 -0800

    css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ const teams = [
     "New York Yankees", "Los Angeles Angels", "Baltimore Orioles", "Texas Rangers", "Tampa Bay Devil Rays",
     "Boston Red Sox", "Minnesota Twins", "Miami Marlins", "Cincinnati Reds", "Pittsburgh Pirates",
     "Cleveland Indians", "Texas Rangers", "Chicago Cubs", "Seattle Mariners", "Oakland Athletics",
-    "Philadelphia Eagles", "Dallas Cowboys", "Carolina Panthers", "Seattle Seahawks", "Green Bay Packers", 
+    "Philadelphia Eagles", "Dallas Cowboys", "Carolina Panthers", "Seattle Seahawks", "Green Bay Packers",
     "Los Angeles Rams", "New York Jets", "Pittsburgh Steelers", "Minnesota Vikings", "New England Patriots",
-    "Washington Redskins", "Cleveland Browns", "New Orleans Saints", "Houston Texans", "Buffalo Bills", 
+    "Washington Redskins", "Cleveland Browns", "New Orleans Saints", "Houston Texans", "Buffalo Bills",
     "Jacksonville Jaguars", "Tennessee Titans", "Tampa Bay Buccaneers", "Indianapolis Colts", "Miami Dolphins",
-    "Boston Celtics", "Phoenix Suns", "Houston Rockets", "Denver Nuggets", "Kansas City Chiefs", "Sacramento Kings", 
+    "Boston Celtics", "Phoenix Suns", "Houston Rockets", "Denver Nuggets", "Kansas City Chiefs", "Sacramento Kings",
     "Cleveland Caveliers", "San Antonio Spurs", "Toronto Raptors", "Oklahoma City Thunder", "Utah Jazz",
     "Charlotte Hornets", "Portland Trail Blazers", "Orlando Magic", "Memphis Grizzlies", "Chicago Bulls",
     "Golden State Warriors", "Los Angeles Lakers", "New York Knicks", "Miami Heat", "Milwaukee Bucks",
@@ -18,92 +18,95 @@ const teams = [
     "Los Angeles Galaxy", "New York RedBulls", "Atlanta United FC", "Seattle Sounders FC", "Columbus Crew SC",
     "Orlando City SC", "Sporting Kansas City", "Portland Timbers", "Montreal Impact", "New England Revolution",
     "San Jose Earthquakes", "Houston Dynamo", "Colorado Rapids", "Philadelphia Union", "Real Salt Lake",
-    "Minnesota United FC", "Colorado Rapids", "District Columbia United", "FC Dallas" 
+    "Minnesota United FC", "Colorado Rapids", "District Columbia United", "FC Dallas"
 ];
 
-var winNum = $('#winnum');
-var lossNum = $('#lossnum');
+var winNum = $("#winnum");
+var lossNum = $("#lossnum");
+
 //randomly chooses a team
 var randomNumber = Math.floor(Math.random() * teams.length);
 var chosenTeam = teams[randomNumber].toLowerCase();
 var rightTeam = [];
-var wrongTeam =[];
+var wrongTeam = [];
 var underScoreArray = [];
 var underScore = "";
 var checkArray = [];
+
 //max number of wrong guesses
 var guesses = 5;
-//actual number of times user gets it wrong
+
+//number of times user gets it wrong
 var tries = 0;
 
 //dom manipulation
-var docScore = document.getElementsByClassName('underScore');
-var docRightTeam = document.getElementsByClassName('right');
-var docWrongTeam = document.getElementsByClassName('wrong');
+var docScore = document.getElementsByClassName("underScore");
+var docRightTeam = document.getElementsByClassName("right");
+var docWrongTeam = document.getElementsByClassName("wrong");
 
 //create underscores based on word length
 var generateScore = () => {
-    for(let i = 0; i < chosenTeam.length; i++) {
+    for (let i = 0; i < chosenTeam.length; i++) {
         if (' \t\n\r\v'.indexOf(chosenTeam[i]) > -1) {
             underScoreArray.push(' ');
         } else {
             underScoreArray.push('_');
         }
-        
     }
     underScore = underScoreArray.join('&nbsp;');
     console.log(underScore);
 }
 
-setVars = () => {
+var setVars = () => {
     //randomly chooses a team
     randomNumber = Math.floor(Math.random() * teams.length);
     chosenTeam = teams[randomNumber].toLowerCase();
     rightTeam = [];
-    wrongTeam =[];
+    wrongTeam = [];
     underScoreArray = [];
     underScore = "";
     checkArray = [];
+
     //max number of wrong guesses
     guesses = 5;
+
     //actual number of times user gets it wrong
-    tries = 0;        
+    tries = 0;
 }
 
 var resetGame = () => {
     setVars();
-    $('.underScore').text('')
-    $('.right').text('~Correct~');
-    $('.wrong').text('~Wrong~');
+    $(".underScore").text("");
+    $(".right").text("~Correct Letters~");
+    $(".wrong").text("~Wrong Letters~");
     generateScore();
     docScore[0].innerHTML = underScore;
 }
 
 //get users guess
-document.addEventListener('keypress', (event) => {
+document.addEventListener("keypress", (event) => {
     var keyword = String.fromCharCode(event.keyCode);
     console.log(keyword);
 
     // if user has guesses left
     if (tries >= guesses) {
-        alert('You lose');
+        alert("You lost :(");
         console.log(lossNum.text());
         lossNum.text(parseInt(lossNum.text()) + 1);
         resetGame();
         return;
     }
-    
-    //if user guess is right
-    if(chosenTeam.indexOf(keyword) > -1) {
 
+    //if user guess is right
+    if (chosenTeam.indexOf(keyword) > -1) {
 
         //add to right teams array
-        if(rightTeam.indexOf(keyword) < 0) {
+        if (rightTeam.indexOf(keyword) < 0) {
             rightTeam.push(keyword);
         }
-        
+
         //find the indexes of each necessary underscore
-        for(i = 0; i < chosenTeam.length; i++) {
+        for (i = 0; i < chosenTeam.length; i++) {
             console.log(keyword === chosenTeam[i]);
             if (keyword === chosenTeam[i]) {
                 underScoreArray.splice(i, 1, keyword);
@@ -127,27 +130,24 @@ document.addEventListener('keypress', (event) => {
         console.log(underScoreArray);
         console.log(chosenTeam);
         console.log(underScoreArray.join(""));
-        
+
         docScore[0].innerHTML = underScore;
 
         if (underScoreArray.join("") === chosenTeam) {
-            alert('You win');
+            alert("You're a sports genius!");
             winNum.text(parseInt(winNum.text()) + 1);
             resetGame();
         }
-        
-        //After the user wins/loses the game should automatically choose another word
-        //Project an image of a sports star when user wins 
-        
     }
     else {
-        if(wrongTeam.indexOf(keyword) < 0) {
-        wrongTeam.push(keyword);
-        docWrongTeam[0].innerHTML = wrongTeam;
-        tries++;
-        console.log(tries);
+        if (wrongTeam.indexOf(keyword) < 0) {
+            wrongTeam.push(keyword);
+            docWrongTeam[0].innerHTML = wrongTeam;
+            tries++;
+            console.log(tries);
         }
     }
+
 });
 
 generateScore();
